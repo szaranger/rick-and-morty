5a77debcc962d71669b2bf5b5b7a677060bd69fd
Sean Saranga Amarasinghe <szaranger@gmail.com>
2025-04-05 09:03:50 +1100

    chore: initial commit with Next.js and Chakra-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
+import { Provider } from "@/components/ui/provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
